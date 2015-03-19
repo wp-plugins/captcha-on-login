69957053be04aaf57cda4f23eff1eb2498961a5f
Anderson.Makiyama <Anderson.Makiyama@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-19 04:12:07 +0000

    version 2.1 publication
    git-svn-id: https://plugins.svn.wordpress.org/captcha-on-login/tags/2.1@1115769 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/report.php
@@ -0,0 +1,189 @@
+<div class="wrap">
+<div class="icon32"><img src='<?php echo plugins_url('/images/icon-32.png', dirname(__FILE__))?>' /></div>
+        
+<h2><?php echo self::PLUGIN_NAME?> <?php echo __('Reports', self::CLASS_NAME)?>:</h2>
+    
+  		<table width="100%"><tr>
+        <td style="vertical-align:top">
+ 
+ 		
+        
+        <div class="metabox-holder">         
+		<div class="postbox" >
+        	<h3><?php _e('Last 1000 successfull or failed logins',self::CLASS_NAME);?></h3>
+        
+        	<div class="inside">
+            
+            <label><?php _e('IP&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;Date and Time&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;Status',self::CLASS_NAME)?></label>
+            
+                <p>
+                <div style="width:600px;height:300px; overflow:scroll;">
+                	<?php
+					foreach($last_100_logins as $login):
+					?>
+                    <?php echo implode("&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;",$login)?><br>
+                    <?php
+					endforeach;
+					?>
+                </div>
+                </p>
+
+			</div>
+		</div>
+        </div>
+ 
+ 
+ 		<div class="metabox-holder">
+ 		<div class="postbox">
+        
+        	<h3><?php _e('All Currently Blocked IPs', self::CLASS_NAME);?></h3>
+        
+        	<div class="inside">
+            
+            	<table width="100%">
+                <tr>
+                <td width="50%">
+                <h4><?php _e('Temporary Blocked IPs', self::CLASS_NAME);?></h4>
+            	
+            	<p>
+                    <div style="width:350px;height:300px; overflow:scroll;">
+                        <?php
+                        foreach($ips as $ip):
+                        ?>
+                        <?php echo $ip[0];?></br>
+                        <?php
+                        endforeach;
+						
+						if(empty($ips)) echo "<option>" . __('Currently there is no Ips blocked. That is good :)', self::CLASS_NAME) . "</option>";
+                        ?>
+                    </div>
+                </p>
+            	<label><?php _e('Note that temporary blocked IPs are unblocked<br> automatically on the next day!', self::CLASS_NAME);?></label>
+				</td>
+                <td width="50%">
+
+                <h4 style="color:red;"><?php _e('Permanent Blocked IPs', self::CLASS_NAME);?></h4>
+            	
+            	<p>
+                    <div style="width:350px;height:300px; overflow:scroll;">
+                        <?php
+                        foreach($permanent_ips as $p_ip):
+                        ?>
+                        <?php echo $p_ip;?></br>
+                        <?php
+                        endforeach;
+						
+						if(empty($permanent_ips)) echo "<option>" . __('Currently there is no Permanent Ips blocked. That is good :)', self::CLASS_NAME) . "</option>";
+                        ?>
+                    </div>
+                </p>
+                <label><?php _e('Note that permanent blocked IPs keep blocked<br>until you unblock it manually!', self::CLASS_NAME);?></label>                
+                </td>
+                </tr>
+                </table>
+                
+			</div>
+		</div>
+        </div>
+                
+        
+          
+   		</td>
+        <td style="vertical-align:top; width:410px">
+
+        
+
+        <div class="metabox-holder">
+
+		<div class="postbox" >
+    
+
+             <h3 style="font-size:24px; text-transform:uppercase;color:#F00;"><?php _e('Are you an Affiliate?',self::CLASS_NAME)?></h3>
+
+            
+
+             <h3><?php _e('Meet the Most Powerful Affiliate Links MNG',self::CLASS_NAME)?>: <a href="<?php _e('http://plugin-wp.net/hotlinks',self::CLASS_NAME)?>" target="_blank">HotLinks</a></h3>
+
+
+        	<div class="inside">
+
+                <p>
+
+                <a href="http://plugin-wp.net/hotlinks" target="_blank"><img src="<?php echo $anderson_makiyama[self::PLUGIN_ID]->plugin_url?>images/hotplus.jpg" ></a>
+
+				</p>
+				
+
+			</div>
+
+ 
+ 		</div>
+        </div>
+        
+        <div class="metabox-holder">
+
+		<div class="postbox" >
+    
+
+             <h3 style="font-size:24px; text-transform:uppercase;color:#F00;"><?php _e('Pay 1, Get 87!',self::CLASS_NAME)?></h3>
+
+            
+
+             <h3>Mega WP Premium Themes PACK: <a href="http://plugin-wp.net/elegantthemes" target="_blank">Elegant Themes</a></h3>
+
+
+        	<div class="inside">
+
+                <p>
+
+                <a href="http://plugin-wp.net/elegantthemes" target="_blank"><img src="<?php echo $anderson_makiyama[self::PLUGIN_ID]->plugin_url?>images/elegantthemes.jpg" ></a>
+
+				</p>
+				
+
+			</div>
+
+ 
+ 		</div>
+        </div>           
+
+       </td>
+       </tr>
+       </table>
+
+
+<hr />
+
+
+<table>
+<tr>
+<td>
+<img src="<?php echo $anderson_makiyama[self::PLUGIN_ID]->plugin_url?>images/anderson-makiyama.png" />
+</td>
+<td>
+<ul>
+
+<li><?php _e('Author',self::CLASS_NAME);?>: <strong>Anderson Makiyama</strong>
+
+</li>
+
+<li><?php _e("Author's email",self::CLASS_NAME);?>: <a href="mailto:andersonmaki@gmail.com" target="_blank">andersonmaki@gmail.com</a>
+
+</li>
+
+<li>
+
+<?php _e("Facebook",self::CLASS_NAME);?>: <a href="https://www.facebook.com/AndersonMaki" target="_blank">Facebook.com/AndersonMaki</a>
+
+</li>
+
+<li><?php _e('Visit the Plugin page',self::CLASS_NAME);?>: <a href="<?php echo self::PLUGIN_PAGE?>" target="_blank"><?php echo self::PLUGIN_PAGE?></a>
+
+</li>
+
+</ul>
+</td>
+</tr>
+</table>
+
+</div>
